9de62e15f2916f9658f1087a58183bd7e0be6034
diplomatapina <pina@diplomatadelicatessen.com.br>
2026-03-09 11:44:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ def conciliar():
     return f"Conciliação iniciada para a loja: {loja}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
